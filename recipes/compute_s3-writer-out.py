34ff3aa7add2237caff2f08f5548aa77575c7869
admin <admin>
2021-05-26 16:09:03 +0000

    Saved recipe: CONNECTIONTEST.compute_s3-writer-out

--- recipes/compute_s3-writer-out.py
+++ recipes/compute_s3-writer-out.py
@@ -10,7 +10,7 @@ s3_reader_in_df = s3_reader_in.get_dataframe()
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc. 
 
 s3_writer_out_df = s3_reader_in_df # For this sample code, simply copy input to output
 
